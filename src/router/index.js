fd7148795973d76b626a9eb1da4906b9bbec088f
haodakouqi <haodakouqi@126.com>
2026-03-04 20:07:27 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import dashboard from '../pages/dashboard/index.vue'
 
-import Home from '../pages/Home.vue'
-import PrivacyPolicy from '../pages/common/PrivicyPolicy.vue'
-import YarnCase from '../pages/case/yarn.vue'
-import OnCourtCase from '../pages/case/oncourt.vue'
-import herbmedCase from '../pages/case/herbmed.vue'
-import VuxenCase from '../pages/case/vuxen.vue'
-import LeroyMerlinCase from '../pages/case/leroyMerlin.vue'
-import WestwingCase from '../pages/case/westwing.vue'
+// import Home from '../pages/Home.vue'
+// import PrivacyPolicy from '../pages/common/PrivicyPolicy.vue'
+// import YarnCase from '../pages/case/yarn.vue'
+// import OnCourtCase from '../pages/case/oncourt.vue'
+// import herbmedCase from '../pages/case/herbmed.vue'
+// import VuxenCase from '../pages/case/vuxen.vue'
+// import LeroyMerlinCase from '../pages/case/leroyMerlin.vue'
+// import WestwingCase from '../pages/case/westwing.vue'
 
 const routes = [
   {
@@ -60,55 +60,50 @@ const routes = [
     name: 'test',
     component: () => import('../pages/test.vue')
   },
-  {
-    path: '/Home',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/contact',
-    component: Home,
-  },
   // {
-  //   path: '/home',
+  //   path: '/Home',
   //   name: 'Home',
   //   component: Home
   // },
-  {
-    path: '/privacy-center',
-    name: 'PrivacyPolicy',
-    component: PrivacyPolicy
-  },
-  {
-    path: '/case/yarn',
-    name: 'YarnCase',
-    component: YarnCase
-  },
-  {
-    path: '/case/oncourt',
-    name: 'OnCourtCase',
-    component: OnCourtCase
-  },
-  {
-    path: '/case/herbmed',
-    name: 'HerbmedCase',
-    component: herbmedCase
-  },
-  {
-    path: '/case/vuxen',
-    name: 'VuxenCase',
-    component: VuxenCase
-  },
-  {
-    path: '/case/leroyMerlin',
-    name: 'LeroyMerlinCase',
-    component: LeroyMerlinCase
-  },
-  {
-    path: '/case/westwing',
-    name: 'WestwingCase',
-    component: WestwingCase
-  }
+  // {
+  //   path: '/contact',
+  //   component: Home,
+  // },
+  // {
+  //   path: '/privacy-center',
+  //   name: 'PrivacyPolicy',
+  //   component: PrivacyPolicy
+  // },
+  // {
+  //   path: '/case/yarn',
+  //   name: 'YarnCase',
+  //   component: YarnCase
+  // },
+  // {
+  //   path: '/case/oncourt',
+  //   name: 'OnCourtCase',
+  //   component: OnCourtCase
+  // },
+  // {
+  //   path: '/case/herbmed',
+  //   name: 'HerbmedCase',
+  //   component: herbmedCase
+  // },
+  // {
+  //   path: '/case/vuxen',
+  //   name: 'VuxenCase',
+  //   component: VuxenCase
+  // },
+  // {
+  //   path: '/case/leroyMerlin',
+  //   name: 'LeroyMerlinCase',
+  //   component: LeroyMerlinCase
+  // },
+  // {
+  //   path: '/case/westwing',
+  //   name: 'WestwingCase',
+  //   component: WestwingCase
+  // }
 ]
 
 const router = createRouter({
